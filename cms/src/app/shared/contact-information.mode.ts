016647d10c6e9eec82982199afe536852e1bb79f
Ecob1 <78009340+Ecob1@users.noreply.github.com>
2024-05-17 21:27:20 -0700

    added contact-item folder

---
+++ cms/src/app/shared/contact-information.mode.ts
@@ -0,0 +1,11 @@
+export class Information {
+
+    constructor(
+    // public id: string,
+    public name: string,
+    public email: string,
+    public phone: string,
+    public imgUrl: string,
+    // public group?: Information[]
+    ) {}
+}
